fdc5fbfb5f409cbfd5ade0c4de67f194d4c33239
Alex Rocha <rocha8524@gmail.com>
2019-11-16 18:38:02 -0500

    Routes Folder in progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,5 @@
+// DATA: Below data will hold all of the friends
+
 var friendsArray = [
     {
         name = "Ahmed",
@@ -5,3 +7,6 @@ var friendsArray = [
         scores = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
 ];
+
+// Export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -11,8 +11,12 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(request, response) {
-    response.sendFile(path.join())
+app.get("/app/public/home.html", function(request, response) {
+    response.sendFile(path.join(__dirname, "home.html"))
+});
+
+app.get("/app/public/survey.html", function(request, response) {
+    response.sendFile(path.join(__dirname, "survey.html"))
 });
 
 // Starts the server to begin listening
