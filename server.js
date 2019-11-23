4b690ec7818beda339eb53293fd6c3380c634047
Alex Rocha <rocha8524@gmail.com>
2019-11-23 12:55:06 -0500

    Fixing pop-up

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 // Modules used 
 var express = require("express");
-var bodyParser = require('body-parser');
 
 //Bootstrapping express
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express.static("app/public"));
 
 // Required to send to routes
 require("./app/routing/apiRoutes")(app);
